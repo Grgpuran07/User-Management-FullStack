12596b4123793a0801c451b407c509f546184779
Puran Gurung <purangrg82@gmail.com>
2023-11-04 14:27:10 +0545

    Add project files.

---
+++ Backend/Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Core.Entities
+{
+    public class BaseEntity<TID>
+    {
+        public TID id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdatedAt { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
